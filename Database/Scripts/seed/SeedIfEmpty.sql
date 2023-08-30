f51bc8bad72344e4a38fc419e2637d32ea81bcd8
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 10:05:56 -0500

    Add SeedIfEmpty post deployment script

---
+++ Database/Scripts/seed/SeedIfEmpty.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[Playground])
+BEGIN
+    INSERT INTO [dbo].[Playground] ([FirstName], [MiddleName], [LastName], [ExamDate], [Price])
+    VALUES
+        ('John', 'James', 'Man', '1992-09-12', 8.99),
+        ('Theo', 'Van', 'Smith', '1992-08-14', 5.99),
+        ('Greg', 'Milton', 'Joon', '1992-11-01', 19.99)
+END
