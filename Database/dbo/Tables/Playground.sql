2c3c58fe303bbe2c67ab244b39da546b05f9e627
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 09:40:51 -0500

    Add Playground table

---
+++ Database/dbo/Tables/Playground.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Playground]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [MiddleName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [ExamDate] DATE NOT NULL, 
+    [Price] DECIMAL(18, 2) NOT NULL
+)
