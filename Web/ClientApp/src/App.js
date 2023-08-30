9b787d22553a74ca5e17dd9461a62f0ca533ad97
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 13:30:54 -0500

    Make ClientApp boilerplate

--- Web/ClientApp/src/App.js
+++ Web/ClientApp/src/App.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
+import React from "react";
+import './App.scss';
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
 
-  render() {
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
     );
-  }
-}
+};
+
+export default App;
