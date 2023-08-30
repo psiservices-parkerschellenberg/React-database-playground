7a35d46999f8d6429c0c9e30bf7e96c7682df374
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 10:57:10 -0500

    Add Row model

---
+++ Web/Models/Row.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.Models
+{
+    public class Row
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "First")]
+        
+        public string? MiddleName { get; set; }
+        [Display(Name = "Middle")]
+        
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Last")]
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime ExamDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
+        public decimal Price { get; set; }
+        
+        public Row()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
