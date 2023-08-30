c317f61ed4482b2d39f44caa0a94f129b9f4be4c
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 10:17:59 -0500

    Add AddRow stored procedure

---
+++ Database/dbo/Stored Procedures/AddRow.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[AddRow]
+    @FirstName VARCHAR(50),
+    @MiddleName VARCHAR(50) = NULL,
+    @LastName VARCHAR(50),
+    @ExamDate DATE,
+    @Price DECIMAL(18, 2)
+AS
+BEGIN
+    INSERT INTO Playground(FirstName, MiddleName, LastName, ExamDate, Price)
+    VALUES (@FirstName, @Middlename, @LastName, @ExamDate, @Price)
+END
