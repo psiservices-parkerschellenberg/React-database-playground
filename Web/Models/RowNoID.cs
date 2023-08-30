9f406403fb2e0ff0783fe9833d2c3396525e48f9
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-30 10:59:35 -0500

    Add RowNoID model

---
+++ Web/Models/RowNoID.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.Models
+{
+    public class RowNoID
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "First")]
+
+        public string? MiddleName { get; set; }
+        [Display(Name = "Middle")]
+
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Last")]
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime ExamDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
+        public decimal Price { get; set; }
+
+        public RowNoID()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
