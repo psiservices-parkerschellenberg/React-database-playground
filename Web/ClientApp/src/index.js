3675d525492c6ae6b81c77795d1243ef710f7ff4
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-31 09:44:26 -0500

    npm i tanstack react-query

--- Web/ClientApp/src/index.js
+++ Web/ClientApp/src/index.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+//import './assets/scss/main.scss';
+//import "react-datepicker/dist/react-datepicker.css";
+//import { BrowserRouter } from 'react-router-dom';
+
+//import "@popperjs/core";
+//import "bootstrap";
+
+//import { HelmetProvider } from 'react-helmet-async';
+
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+//import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            suspense: true,
+        },
+    },
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
         <App />
-    </React.StrictMode>
+    </QueryClientProvider>
 );
