d5b5bb3905201a508127be789fbb773f64ff4ae6
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-08-31 09:51:54 -0500

    npm i tanstack react-query-devtools

--- Web/ClientApp/src/index.js
+++ Web/ClientApp/src/index.js
@@ -15,7 +15,7 @@ import {
     QueryClient,
     QueryClientProvider,
 } from '@tanstack/react-query'
-//import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
 const queryClient = new QueryClient({
@@ -31,5 +31,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 );
