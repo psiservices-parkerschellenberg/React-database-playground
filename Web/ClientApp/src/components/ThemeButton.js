e63a8d4ce976c5d7f92ccaf1be425d5647726296
psiservices-parkerschellenberg <parker.schellenberg@psionline.com>
2023-09-01 12:17:49 -0500

    Create ThemeButton.js

---
+++ Web/ClientApp/src/components/ThemeButton.js
@@ -0,0 +1,20 @@
+﻿import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+const ThemeButton = () => {
+
+    const [theme, setTheme] = useState('light'); // 'light' or 'dark'
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+
+        document.documentElement.setAttribute('data-bs-theme', newTheme);
+    };
+
+    return (
+        <Button onClick={toggleTheme}>Toggle Theme</Button>
+    );
+};
+
+export default ThemeButton;
